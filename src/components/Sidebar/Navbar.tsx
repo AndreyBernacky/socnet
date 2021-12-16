ba1e9d086cf804055b00688644ecc1f0e97fd3df
AndreyBernacky <dronskmne@gmail.com>
2021-12-16 20:18:24 +0300

    20 add NavLink

--- src/components/Sidebar/Navbar.tsx
+++ src/components/Sidebar/Navbar.tsx
@@ -1,15 +1,16 @@
 import React from "react";
 import s from './Navbar.module.css';
+import {NavLink} from 'react-router-dom'
 
 export const Navbar = () => {
     return(
         <nav className={s.nav}>
             <ul className={s.navMenu}>
-                <li><a href={'/profile'}>Profile</a></li>
-                <li><a href={'/dialogs'}>Messages</a></li>
-                <li><a href={'/News'}>News</a></li>
-                <li><a href={'/Music'}>Music</a></li>
-                <li><a href={'/Settings'}>Settings</a></li>
+                <li><NavLink to={'/profile'} className={({isActive}) => `${isActive ? s.navMenuActive : ''}`}>Profile</NavLink></li>
+                <li><NavLink to={'/dialogs'} className={({isActive}) => `${isActive ? s.navMenuActive : ''}`}>Messages</NavLink></li>'
+                <li><NavLink to={'/News'} className={({isActive}) => `${isActive ? s.navMenuActive : ''}`}>News</NavLink></li>
+                <li><NavLink to={'/Music'} className={({isActive}) => `${isActive ? s.navMenuActive : ''}`}>Music</NavLink></li>
+                <li><NavLink to={'/Settings'} className={({isActive}) => `${isActive ? s.navMenuActive : ''}`}>Settings</NavLink></li>
             </ul>
         </nav>
     )
