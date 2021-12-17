0b5f82f190d8e883be8f887e10a776b3c3883192
AndreyBernacky <dronskmne@gmail.com>
2021-12-17 18:31:20 +0300

    page Dialog

--- src/components/Dialogs/Dialogs.tsx
+++ src/components/Dialogs/Dialogs.tsx
@@ -1,7 +1,37 @@
+import s from './Dialog.module.css';
+
 export const Dialogs = () => {
     return (
-        <div>
-            Dialogs Page
+        <div className={s.dialogsWrap}>
+            <div className={s.dialogs}>
+                <h1>Dialogs</h1>
+                <div className={s.dialogItem + ' ' + s.active}>
+                    Alex
+                </div>
+                <div className={s.dialogItem}>
+                    Igor
+                </div>
+                <div className={s.dialogItem}>
+                    Andrey
+                </div>
+                <div className={s.dialogItem}>
+                    Viktor
+                </div>
+                <div className={s.dialogItem}>
+                    Dmitry
+                </div>
+            </div>
+            <div className={s.messages}>
+                <div className={s.message}>
+                    Hi!
+                </div>
+                <div className={s.message}>
+                    What are you doing?
+                </div>
+                <div className={s.message}>
+                    I learn React
+                </div>
+            </div>
         </div>
     )
 }
